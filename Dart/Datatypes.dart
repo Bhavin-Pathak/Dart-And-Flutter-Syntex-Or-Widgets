af452de6a06a1d8cbb993c31413ac03e55f4eb92
Bhavin Pathak <bupadhyay277@gmail.com>
2024-02-24 15:33:09 +0530

    Datatypes in Dart

---
+++ Dart/Datatypes.dart
@@ -0,0 +1,24 @@
+//! Datatypes In Dart Programmimg Language
+
+/*
+>Numbers
+>String
+>Boolean
+>List
+>Sets
+>Maps
+>Runes
+>Symbols
+>Numbers
+>Numbers
+*/
+
+//! Number Datatypes Has 2 Types
+//? Int  :-   Isme Main Number Ko Store Karte Hai Like :: 1,2,3,4,5,6,7,8,9,10   Declaretion = Int a = 5;
+//? Double :- Isme Main Number Ko Store Karte Hai Lakin Diffrent Type Se Like :: 1.1, 2.2, 3.2, 4.2, 5.2  Declaretion = Double a = 1.1;
+
+//! String Datatypes
+//? String :- Isme Silply "" Double Cots Me Value Define Karte Hai Like : "BHAVIN" Declaretion = String name = "BHAVIN";
+
+//! Boolean Datatypes
+//? Boolean :- Isme True Or False Ka Value Dekha Jayega like : bool isSelected = true;
