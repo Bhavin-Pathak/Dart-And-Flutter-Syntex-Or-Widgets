6f970a067a65cd177e8a449c3df196451dc22378
Bhavin Pathak <bupadhyay277@gmail.com>
2024-02-27 12:36:08 +0530

    Chore: Data Types Added

---
+++ Dart/Main.dart
@@ -0,0 +1,14 @@
+void main() {
+  //! INT Data Type :- That Contains Intiger Values Like 1 to 9
+  int a = 10;
+  print(a);
+  //! String Data Type :- That Contains String Characters Inside "" '' Like Name Peregraphs etc
+  String name = "BHAVIN";
+  print(name);
+  //! Double Data Type :- That Contains Double Values Like 1.2,1.5 ETC
+  double value = 2.3;
+  print(value);
+  //! Boolean Data Type :- That Contains Boolean Values Like True or False
+  bool status = true;
+  print(status);
+}
