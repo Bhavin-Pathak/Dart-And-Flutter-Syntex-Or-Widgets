8d5fc591aa832201ac38b64c417e8ba1f6063780
Bhavin Pathak <bupadhyay277@gmail.com>
2024-02-27 14:43:50 +0530

    Chore: Var Keyword In Dart

---
+++ Dart/Var Keyword.dart
@@ -0,0 +1,16 @@
+void main() {
+  //! Var Keyword in Dart
+  //? Var Keyword is variable in Dart without specifying it's data types
+
+  var a = 10;
+  print(a.runtimeType);
+
+  var b = "Bhavin";
+  print(b.runtimeType);
+
+  var c = 2.5;
+  print(c.runtimeType);
+
+  var d = true;
+  print(d.runtimeType);
+}
