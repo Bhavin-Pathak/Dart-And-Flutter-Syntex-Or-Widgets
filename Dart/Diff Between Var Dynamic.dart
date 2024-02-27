bc9b12fb5fd6bcba1188514351dfa3d5a6342b10
Bhavin Pathak <bupadhyay277@gmail.com>
2024-02-27 14:59:26 +0530

    Chore: Diff Added Between Var Or Dynamic Keyword In Dart

---
+++ Dart/Diff Between Var Dynamic.dart
@@ -0,0 +1,12 @@
+void main() {
+  //! Var Variable When We Assign We Do Not Chnage Type
+  var a = 10;
+  a = 20;
+  // a = "a";  Cannt Assign String To Variable
+  print(a);
+  //! Dynamic Variable When We Assign We can Chnage Type on the variable
+  dynamic b = 10;
+  b = "Bhavin";
+  b = "Hell";
+  print(b);
+}
