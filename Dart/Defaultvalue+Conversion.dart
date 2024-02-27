3440e4d2e3103b22315f5033481b23c8ae10d552
Bhavin Pathak <bupadhyay277@gmail.com>
2024-02-27 13:56:29 +0530

    Chore: Conversion Adde

---
+++ Dart/Defaultvalue+Conversion.dart
@@ -0,0 +1,16 @@
+void main() {
+  //!String To Int Convert
+  int a = 5;
+  int b = int.parse("10");
+  print(a + b);
+  //!String To Double Convert
+  int c = 5;
+  double d = double.parse("1.0");
+  print(c + d);
+  //!Int To String Convert
+  String e = 20.toString();
+  print(e.runtimeType);
+  //!Double To String Convert
+  String f = 25.0.toString();
+  print(f.runtimeType);
+}
