43d550d2bb1027b691c88eec4a70489569372cca
Bhavin Pathak <bupadhyay277@gmail.com>
2024-02-27 16:09:14 +0530

    Dif BWN Do or Do-While

---
+++ Dart/diff while or dowhiile.dart
@@ -0,0 +1,15 @@
+void main() {
+  //! Diff Between While Or Do- While loop In Dart
+  print("While Loping:- \n");
+  var i = 100;
+  while (i < 100) {
+    print("Hello Mister ${i}");
+    i++;
+  }
+  print("Do-While Loping:- \n");
+  var j = 200;
+  do {
+    print("Hello Mister ${j}");
+    j++;
+  } while (j < 200);
+}
