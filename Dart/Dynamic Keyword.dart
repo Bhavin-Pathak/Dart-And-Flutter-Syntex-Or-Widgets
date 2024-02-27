349785bc5227f4c2c3c98265d31a50c429aae29e
Bhavin Pathak <bupadhyay277@gmail.com>
2024-02-27 14:53:50 +0530

    Chore: Dynamic Keywords Added

---
+++ Dart/Dynamic Keyword.dart
@@ -0,0 +1,16 @@
+void main() {
+  //! Dynamic Keyword in Dart
+  //? dynamic Keyword is variable in Dart without specifying it's data types but when we need to change data type then chnage it
+
+  dynamic a = 5;
+  print(a);
+
+  a = 15;
+  print(a);
+  a = 2.5;
+  print(a);
+  a = true;
+  print(a);
+  a = "Hello";
+  print(a);
+}
