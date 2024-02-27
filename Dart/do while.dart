685aef296919beb28e4b19c982450aca1530d165
Bhavin Pathak <bupadhyay277@gmail.com>
2024-02-27 15:44:20 +0530

    Chore: Syntex of do while added

---
+++ Dart/do while.dart
@@ -0,0 +1,9 @@
+void main() {
+  //! Do While Is Similer To While Loop But Conditions Cheaked In End Of The Loop
+  var a = 0; // Start Value For Variable 'a'
+  do {
+    print("Hello Do Block $a");
+    a++; // increse the counter each time loop run
+  } while (
+      a < 10); //Its Executed < 10 times Because It Checks After Each Iteration
+}
