9d2d0498a81adc992eeccf0bc58631d9d826f47e
Bhavin Pathak <bupadhyay277@gmail.com>
2024-02-27 15:38:02 +0530

    Chore: While loop Added

---
+++ Dart/while loop.dart
@@ -0,0 +1,14 @@
+void main() {
+  //! While Loop is Similer to For Loop But In This Loop We Assign Expression To Loop
+  /*
+  while (expression){
+    statement;
+  }
+  */
+  var a = 0; //Initial Value
+  while (a < 10) {
+    //Expression Cheak
+    print(a);
+    a++; // Incrise Value By One Each Time When The Code Execute.
+  }
+}
