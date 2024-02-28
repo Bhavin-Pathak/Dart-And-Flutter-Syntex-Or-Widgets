2ddd7af463204850c5f228162d8ec64ac072d653
Bhavin Pathak <bupadhyay277@gmail.com>
2024-02-28 11:38:20 +0530

    Chore: For in loop Added

---
+++ Dart/for in Loop.dart
@@ -0,0 +1,12 @@
+void main() {
+  //! For in Loop In Dart
+  /*
+  for (var_name in object){
+    print(var_name);
+  }
+  */
+  var arry = ["Bhavin", 3, "Bhavin", 4, "Bhavin", 5, "Bhavin", 6];
+  for (var i in arry) {
+    print(i);
+  }
+}
