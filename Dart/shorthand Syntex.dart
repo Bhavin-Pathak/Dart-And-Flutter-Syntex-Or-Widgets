69150da0d0e4b094ebf1293621b1709c87007817
Bhavin Pathak <bupadhyay277@gmail.com>
2024-02-28 10:21:15 +0530

    Chore:  Short hand Syntex added

---
+++ Dart/shorthand Syntex.dart
@@ -0,0 +1,7 @@
+void main() {
+  //! For Function That Contain Just One Expression You can use a shorthand syntex
+  //! => The arrow notation is somtime reffered to as arrow syntex
+  print(addNumbers(20, 60));
+}
+
+int addNumbers(var a, var b) => a + b;  // in thid we dont used 3rd var to addition the values its direct addition by arrow syntex
