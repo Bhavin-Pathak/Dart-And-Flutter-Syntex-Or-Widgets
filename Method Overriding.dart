2d2b2655808e00825eff5c7d67883b50e5ea7b25
Bhavin Pathak <bupadhyay277@gmail.com>
2024-03-04 09:43:01 +0530

    Chore: Method @Overriding Added

---
+++ Method Overriding.dart
@@ -0,0 +1,28 @@
+void main() {
+  //! Method Overriding is the one of most concept in OPPS when we use inheritance then we used method overriding
+  //? Super Class Method Declare In Child Class Nae Or Type Is Same but implimention is modifyed
+  // Sub class override instance method including setter and getter
+  //! Use Can Use @Override annotaction to indicate that u are overriding members
+
+  //! EG
+
+  var object = B();
+  object.DisplayOutput();
+}
+
+class A {
+  // Super Class
+  @override
+  void DisplayOutput() {
+    print("Super Class A");
+  }
+}
+
+class B extends A {
+  // Sub Class
+  @override
+  void DisplayOutput() {
+    super.DisplayOutput();
+    print("Sub Class B");
+  }
+}
