3322a1b7f2563a99cbf1ea50806ff76a76587312
Bhavin Pathak <bupadhyay277@gmail.com>
2024-03-06 14:37:19 +0530

    Chore: Generics Added

---
+++ Dart/Generics.dart
@@ -0,0 +1,9 @@
+void main() {
+  //! Generics in Dart
+  //?  A generic class is a class that can work with different types.
+  // Types safe Collections
+
+  List mylist = [];
+  mylist.add(1);
+  print("List: $mylist");
+}
