ac2bf3fe6b37946ce39f65ba876f7b1905a14095
Bhavin Pathak <bupadhyay277@gmail.com>
2024-03-06 14:29:03 +0530

    Chore: Enums Added

---
+++ Dart/Enums.dart
@@ -0,0 +1,17 @@
+void main() {
+  //! Enums in Dart
+  //? Enums are  used to represent a fixed numbers of constant values
+  /*
+  enum name{
+    value1, // o element value 
+    value2,
+    value3
+  }  
+  */
+
+  for (var b in Data.values) {
+    print(b);
+  }
+}
+
+enum Data { my, name, bhavin, pathak }
