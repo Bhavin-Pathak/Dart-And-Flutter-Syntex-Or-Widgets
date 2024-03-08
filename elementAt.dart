f960b0e1656ed03be44ab9dacc3e225dbdfc47b1
Bhavin Pathak <bupadhyay277@gmail.com>
2024-03-08 15:07:08 +0530

    Chore: Element At Added

---
+++ elementAt.dart
@@ -0,0 +1,8 @@
+void main() {
+  //? Element At in Dart
+  List<int> numbers = [1, 2, 3, 4, 5];
+  print("Before elementAT $numbers");
+
+  var result = numbers.elementAt(3);
+  print("After elementAT $result");
+}
