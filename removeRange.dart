c83255854c7f32876542397b5c856907eb287ade
Bhavin Pathak <bupadhyay277@gmail.com>
2024-03-08 12:19:30 +0530

    Chore: Remove Range Added

---
+++ removeRange.dart
@@ -0,0 +1,31 @@
+void main() {
+  //? Remove Range In Dart
+
+  List<int> list = [
+    1,
+    3,
+    43,
+    645,
+    235,
+    235,
+    346,
+    457,
+    568,
+    34,
+    23,
+    4235,
+    346,
+    347,
+    34,
+    532,
+    5,
+    236,
+    326,
+    32623
+  ];
+  print("Before RemoveRange $list");
+  list.removeRange(1, 3);
+  print("After RemoveRange $list");
+
+  // Remove Range Of Elements in list (Start Index , End Index)
+}
