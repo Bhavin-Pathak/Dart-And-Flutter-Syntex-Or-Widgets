23c74f7c3c244b2650bfe07715695be0ef2f6607
Bhavin Pathak <bupadhyay277@gmail.com>
2024-03-08 12:10:15 +0530

    Chore: Single Where Added

---
+++ SingleWhere.dart
@@ -0,0 +1,26 @@
+void main() {
+  //! Single Where In Dart
+  List<int> list = [
+    1,
+    5,
+    4,
+    32,
+    545,
+    6,
+    57,
+    3,
+    5456,
+    7867,
+    56769,
+    345,
+    23,
+    3264,
+    658,
+    679,
+    96,
+    2
+  ];
+  print("Original $list");
+  List<int> result = list.where((element) => element != 0).toList();
+  print("Result $result");
+}
