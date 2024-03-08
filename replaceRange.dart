d561a297f506272bc8c8e4ab13f11f9246a854f1
Bhavin Pathak <bupadhyay277@gmail.com>
2024-03-08 12:37:41 +0530

    Chore: Replace Rnage Added

---
+++ replaceRange.dart
@@ -0,0 +1,11 @@
+void main() {
+  //? Replace Range In Dart
+  //! Replace The Range of elements with the elements of replacement removed the objects in the range from start to end then insert the element
+  // from start to end into the range
+  var list = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+  list.replaceRange(1, 3, [100, 200, 300]);
+  print(list);
+
+  //? Remove Range In Dart
+  //(StartIndex, Endindex, New Elements)
+}
