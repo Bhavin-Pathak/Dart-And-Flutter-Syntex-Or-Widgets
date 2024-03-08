0c17d8133c634124c340b49cba520fea58f64772
Bhavin Pathak <bupadhyay277@gmail.com>
2024-03-08 12:13:40 +0530

    Chore: Remove LAst Added

---
+++ RemoveLast.dart
@@ -0,0 +1,8 @@
+void main() {
+  //? Remove Last Method In List
+  List<int> list = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14];
+  print("Main List: $list");
+  list.removeLast();
+  print("RemovedLast List: $list");
+  //! That Remove Elements From List That Are in Last Position
+}
