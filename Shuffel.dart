0e9bd83ad47e9b3ebae4c4c21026880271ebe7d2
Bhavin Pathak <bupadhyay277@gmail.com>
2024-03-08 11:49:20 +0530

    Chore: Shuffled Added

---
+++ Shuffel.dart
@@ -0,0 +1,36 @@
+void main() {
+  //! Shuffle in Dart
+  List<int> list = [
+    1,
+    2,
+    3,
+    4,
+    5,
+    6,
+    7,
+    8,
+    0,
+    8,
+    7,
+    467,
+    568,
+    647879,
+    78,
+    5674,
+    245,
+    34,
+    5245,
+    4365,
+    567,
+    6887,
+    8978,
+    953,
+    673,
+    64213,
+    51
+  ];
+  print("Orignal: $list");
+  list.shuffle();
+  print("Shuffled: $list");
+  //? Shuffeld Element Rendoms
+}
