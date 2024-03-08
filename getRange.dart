c4b3cfde54fb4cb7a33b482a086ae61bfd7f544f
Bhavin Pathak <bupadhyay277@gmail.com>
2024-03-08 15:23:06 +0530

    Chore: getRange Added

---
+++ getRange.dart
@@ -0,0 +1,7 @@
+void main() {
+  //? Get Range In Dart
+  List<String> names = ["BHAVIN", "MAYUR", "HIMANSHU"];
+  print(names);
+  var result = names.getRange(0, 2); //Start Index or Last Index
+  print(result);
+}
