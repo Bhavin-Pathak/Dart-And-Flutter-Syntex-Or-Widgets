c94cb8780730647a5b5be2ee63648bfee3f3e5b5
Bhavin Pathak <bupadhyay277@gmail.com>
2024-03-08 14:24:17 +0530

    Chore: LastIndex Of Added

---
+++ LastIndexOf.dart
@@ -0,0 +1,30 @@
+void main() {
+  //? Last Index Of In Dart
+
+  List<int> list = [
+    1,
+    2,
+    3,
+    4,
+    5,
+    6,
+    7,
+    9,
+    30,
+    23,
+    40,
+    45,
+    23,
+    12,
+    54,
+    21,
+    900
+  ];
+  print("BeforeLastIndexof $list");
+
+  var result = list.lastIndexOf(54);
+  print("AfterLastIndexof $result");
+
+  //? LastindexOf Method :-
+  // The Starting position of the last match pettern in string Find Match Pettern By Searching Backword Starting at Start
+}
