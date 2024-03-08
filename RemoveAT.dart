be903f152e93a130c805d76c5cd6d355d91231a2
Bhavin Pathak <bupadhyay277@gmail.com>
2024-03-08 12:16:32 +0530

    Chore: removeAT Added

---
+++ RemoveAT.dart
@@ -0,0 +1,9 @@
+void main() {
+  //? Remove At
+
+  List<String> names = ["Bhavin", "Mayur", "Himanshu"];
+  print("Before Removing At $names");
+  names.removeAt(0);
+  print("After Removing at Index  1: ${names}");
+  //? Thats Remmove Elements From List To List By index Number
+}
