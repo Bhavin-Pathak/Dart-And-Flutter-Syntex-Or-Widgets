c2e48926336edbf27f812f6fa69c371ae9741707
Bhavin Pathak <bupadhyay277@gmail.com>
2024-03-08 11:46:02 +0530

    Chore: Sort Method Added

---
+++ Sort.dart
@@ -0,0 +1,25 @@
+void main() {
+  //! Sort in Dart
+  List<int> names = [
+    23,
+    234,
+    234,
+    234,
+    23,
+    5,
+    235,
+    23,
+    423,
+    523,
+    5,
+    231,
+    312,
+    3124,
+    12
+  ];
+  print("Original list: $names");
+
+  names.sort();
+  print("\nSorted list using sort(): $names");
+  //? Minimum to Maximum List
+}
