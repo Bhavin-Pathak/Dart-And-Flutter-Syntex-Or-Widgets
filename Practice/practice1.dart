391fe338d9cdbd2e2b858fb949528cdbcc6b1868
Bhavin8289 <bupadhyay277@gmail.com>
2024-07-05 22:59:03 +0530

    init

--- Practice/practice1.dart
+++ Practice/practice1.dart
@@ -1,14 +1,23 @@
 void main() {
   //! This is the main function that is called when the application is started.
 
-  var name = "Bhavin";
-  print(name);
+  //VARIABLE
+  var greetings = "Hello Welcome To Dart Programming";
+  print(greetings);
+  print(greetings.length);
+  print(greetings[0]);
+  print(greetings[1]);
+  print(greetings[2]);
+  print(greetings[3]);
+  print(greetings[4]);
 
-  var newList = [];
-  newList.add(1);
-  newList.add(2);
-  newList.add(3);
-  newList.add(4);
-  newList.add(5);
-  print(newList);
+  //We Can Assign New Variable Value In Var Keyword
+  greetings = "Bhavin";
+  print(greetings);
+
+  //Constants
+  const greet = "Hello Duniya";
+  print(greet);
+  // We Can't Assign New Value To Constants 
+  
 }
