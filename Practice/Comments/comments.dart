5d29b6421ac8133b172ee1dd46833ea46c1b7c61
Bhavin Pathak <dev.pathakbhavin@gmail.com>
2024-07-24 16:02:29 +0530

    chore: comments added

---
+++ Practice/Comments/comments.dart
@@ -0,0 +1,7 @@
+const singleLineComment = "";
+// That is used for SINGLE LINE COMMENT in Dart Programming Language
+
+const multiLineComment = "";
+ /* 
+That is MULTILINE COMMENT Used For Comment Multiple lines at times 
+*/
