1bf8bd6e64ac147c7f9830ee6f1102451226bc15
Bhavin Pathak <dev.pathakbhavin@gmail.com>
2024-07-24 23:19:52 +0530

    "Added Dart code for variable declarations with examples of var, const, int, String, bool, and double data types."

---
+++ Practice/Variables/variables.dart
@@ -0,0 +1,22 @@
+// VAR Keyword : Variable is used to store values
+
+var name = "Bhavin";
+// var Keyword -> variable name -> = asignoperator -> values
+
+const sname = "";
+// const Keyword -> constant name -> = asignoperator -> values
+// we cannot assign new value to constants
+
+/* HERE IS EXAMPLE OF TYPE ANOTATION OF VARIABLES */
+
+int number = 20; // To Store Numeric Values
+// int datatype -> variable name -> = asignoperator -> values
+
+String greeting = "Hello World!"; // To Store String Values
+// String datatype -> variable name -> = asignoperator -> values
+
+bool isTrue = true; // To Store Boolean Values (TRUE,FALSE)
+// boolean datatype -> variable name -> = asignoperator -> values
+
+double values = 2.2; // To Store Floating Point Numeric Values
+// double datatype -> variable name -> = asignoperator -> values
