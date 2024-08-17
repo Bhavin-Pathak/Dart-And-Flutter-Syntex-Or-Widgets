2fef1360fa0668f9895745316cc114cebaeaced9
Bhavin Pathak <dev.pathakbhavin@gmail.com>
2024-08-17 10:26:11 +0530

    -

--- Practice/if-else.dart
+++ Practice/if-else.dart
@@ -1,21 +1,3 @@
-void main() {
-  A a = A();
-  a.gettingAge();
-  B b = B();
-  b.gettingAge(); // Output: Age: 20
-}
-
-class A {
-  int age = 10;
-  void gettingAge() {
-    print('Age: $age');
-  }
-}
-
-class B extends A {
-  int age = 20;
-  @override
-  void gettingAge() {
-    print('Age: ${super.age}');
-  }
+void main(){
+  //Comments
 }
